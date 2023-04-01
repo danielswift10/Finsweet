0091010f237fd9af0935a2219289ebea33172270
danielswift10 <ojeyomi.daniel@gmail.com>
2023-04-01 15:23:25 +0100

    feat: added assets

---
+++ src/Utils/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ styles }) => {
+    return ( 
+        <button type="button" className={`py-4 hover:scale-[1.03] px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+            Get Started
+        </button>
+     );
+}
+ 
+export default Button;
