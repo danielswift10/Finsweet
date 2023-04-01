9feaa267503250ef429aa09ba0e08f0df559a245
danielswift10 <ojeyomi.daniel@gmail.com>
2023-04-01 21:27:21 +0100

    feat: added NavLinks

---
+++ src/Constants/index.js
@@ -0,0 +1,32 @@
+const NavLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "service",
+    title: "Service",
+  },
+  {
+    id: "company",
+    title: "Company",
+  },
+  {
+    id: "career",
+    title: "Career",
+  },
+  {
+    id: "blog",
+    title: "Blog",
+  },
+  {
+    id: "contact",
+    title: "Contact us",
+  },
+  {
+    id: "blog",
+    title: "Blog",
+  },
+];
+
+export { NavLinks };
