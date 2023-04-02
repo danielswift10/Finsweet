95d05b023a0ce2834ad2f56f0f0ffb1116eef44b
danielswift10 <ojeyomi.daniel@gmail.com>
2023-04-02 23:42:08 +0100

    style: added styling to this component

--- src/Components/Home/Expertise/Expertise.jsx
+++ src/Components/Home/Expertise/Expertise.jsx
@@ -6,18 +6,18 @@ const Expertise = () => {
     <section className="py-10 bg-lightCyan">
       <div className={` ${styles.boxWidth} ${styles.padding} `}>
         <div
-          className={`${layout.section2} about-us ${styles.flexStartB} md:items-center gap-10 md:gap-10`}
+          className={`${layout.section2} about-us ${styles.flexStartB} md:items-start gap-10 md:gap-10`}
         >
           <div>
             <p className={`${styles.caption} mb-3`}>OUR EXPERTISE</p>
             <h2
-              className={`${styles.heading2} mb-5 text-[30px] lg:w-[90%] sm:text-[35px] lg:text-[40px] xl:w-[640px] smd:w-[700px] md:w-full  text-black`}
+              className={`${styles.heading2} mb-5 text-[30px] lg:w-[90%] sm:text-[35px] md:w-[650px] lg:text-[40px] xl:w-[640px] smd:w-[700px] md:w-full  text-black`}
             >
               We want to get local identification in every corner of the world
               in this era of global citizenship
             </h2>
             <p
-              className={`${styles.paragraph} text-gray xl:w-[622px] xs:text-[17px] lg:w-[90%] md:w-[] sm:text-[18px] md:text-[16px] smd:w-[650px] md:w-full`}
+              className={`${styles.paragraph} text-gray xl:w-[622px] xs:text-[17px] md:w-[780px] lg:w-[90%] md:w-[] sm:text-[18px] md:text-[16px] smd:w-[650px] md:w-full`}
             >
               Through True Rich Attended does no end it his mother since real
               had half every him case in packages enquire we up ecstatic
--- src/Components/Home/Services/Services.jsx
+++ src/Components/Home/Services/Services.jsx
@@ -1,13 +1,24 @@
-import styles from "../../../styles";
+import styles, { layout } from "../../../styles";
 
 const Services = () => {
-    return ( 
-        <section className="bg-chalk py-10 ">
-            <div className={` ${styles.boxWidth} ${styles.padding} `}>
-
+  return (
+    <section className="bg-chalk py-10 ">
+      <div className={` ${styles.boxWidth} ${styles.padding} `}>
+        <div
+          className={`${layout.section2} about-us ${styles.flexStartB} md:items-start gap-10 md:gap-10`}
+        >
+            <div>
+            <p className={`${styles.caption} mb-3`}>OUR SERVICES</p>
+            <h2
+              className={`${styles.heading2} mb-5 text-[30px] sm:text-[35px] md:w-[650px] lg:text-[40px] xl:w-[777px] smd:w-[700px] md:w-full  text-black`}
+            >
+              We build software solutions that solve client's business challenges
+            </h2>
             </div>
-        </section>
-     );
-}
- 
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default Services;
