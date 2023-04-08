5e08d66589870820e07ba33d4a00041a8e9abca1
danielswift10 <ojeyomi.daniel@gmail.com>
2023-04-08 02:15:08 +0100

    style: added styling

--- src/Components/Service/WhatWeDo/WhatWeDo.jsx
+++ src/Components/Service/WhatWeDo/WhatWeDo.jsx
@@ -6,12 +6,18 @@ const WhatWeDo = () => {
     <section>
       <div className={`${styles.boxWidth} ${styles.padding}`}>
         {services.map((service, index) => (
-          <div>
-            <div className="content">
-                
+          <div key={service.id}
+          className={`flex mb-5 justify-between  border gap-10 items-center ${
+            index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'
+          } ${index % 2 !== 0 ? 'bg-black' : 'bg-white'} p-4`}
+          >
+            <div className="content flex-1">
+                <h4 className={`${styles.heading4}`}>{service.caption}</h4>
+                <h3 className={`${styles.heading3}`}>{service.title}</h3>
+                <p className={`${styles.paragraph}`}>{service.content}</p>
             </div>
-            <div className="image">
-                <img src={service.image} alt="" />
+            <div className="image flex-1">
+                <img src={service.image} alt="" className="w-fit"/>
             </div>
           </div>
         ))}
