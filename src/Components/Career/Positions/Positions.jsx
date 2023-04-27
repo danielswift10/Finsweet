87aae24fd68fb7381a500be795d3da12956a3d37
danielswift10 <ojeyomi.daniel@gmail.com>
2023-04-27 15:58:04 +0100

    fix: made some changes

--- src/Components/Career/Positions/Positions.jsx
+++ src/Components/Career/Positions/Positions.jsx
@@ -3,7 +3,7 @@ import { position } from "../../../Constants";
 import { arrowRightDark } from "../../../Assets";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
-import JobApplication from "../JobApplication/";
+import JobApplication from "../";
 
 const Positions = () => {
     const [selectedPosition, setSelectedPosition] = useState(null);
