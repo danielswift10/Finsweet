01b33057ddfd4b4b73a371672dc7f9d881106c85
danielswift10 <ojeyomi.daniel@gmail.com>
2023-05-09 16:30:32 +0100

    fix: made some changes

--- src/Utils/Button.jsx
+++ src/Utils/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({title, type}) => {
         <button type={type} className={`lg:h-[64px] w-[197px] relative h-[55px]  lg:w-[219px] hover:scale-[1.03] outline-none ${layout.button}`}>
             <p>{title}</p>
             <img src={arrowRight} alt="arrow-right" />
-            <img src={shapesLeft} className="absolute top-0 left-0 w-[23px] h-[23px] md:w-[28px] md:h-[28px] " alt="" />
+            <img src={shapesLeft} className="absolute top-0 left-0 w-[23px] h-[23px] md:w-[28px] md:h-[28px] " alt="shapes-left" />
         </button>
      );
 }
